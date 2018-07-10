fc5e9ccbcb5592cb2167df18a4fd3d8dae6e9019
Hasin Hayder <countdraculla@gmail.com>
2018-07-10 16:45:56 +0600

    category link

--- template-parts/blog-home/featured.php
+++ template-parts/blog-home/featured.php
@@ -11,13 +11,15 @@ $post_data = array();
 while ( $philosophy_fp->have_posts() ) {
     $philosophy_fp->the_post();
     $categories = get_the_category();
+    $category = $categories[mt_rand(0,count($categories)-1)];
     $post_data[] = array(
         "title" => get_the_title(),
         "date"=>get_the_date(),
         "thumbnail"=>get_the_post_thumbnail_url(get_the_ID(),"large"),
         "author"=>get_the_author_meta("display_name"),
         'author_avatar'=>get_avatar_url(get_the_author_meta("ID")),
-        'cat'=>$categories[mt_rand(0,count($categories)-1)]->name
+        'cat'=>$category->name,
+        'catlink'=>get_category_link($category)
     );
 }
 if ( $philosophy_fp->post_count > 1 ):
@@ -32,7 +34,7 @@ if ( $philosophy_fp->post_count > 1 ):
                          style="background-image:url('<?php echo esc_url($post_data[0]['thumbnail']) ?>');">
 
                         <div class="entry__content">
-                            <span class="entry__category"><a href="#0"><?php echo esc_html($post_data[0]['cat']) ?></a></span>
+                            <span class="entry__category"><a href="<?php echo esc_url($post_data[$i]['catlink']); ?>"><?php echo esc_html($post_data[0]['cat']) ?></a></span>
 
                             <h1>
                                 <a href="#0" title="">
@@ -66,7 +68,7 @@ if ( $philosophy_fp->post_count > 1 ):
                              style="background-image:url('<?php echo esc_url($post_data[$i]['thumbnail']) ?>');">
 
                             <div class="entry__content">
-                                <span class="entry__category"><a href="#0"><?php echo esc_html($post_data[$i]['cat']) ?></a></span>
+                                <span class="entry__category"><a href="<?php echo esc_url($post_data[$i]['catlink']); ?>"><?php echo esc_html($post_data[$i]['cat']) ?></a></span>
 
                                 <h1>
                                     <a href="#0" title="">
